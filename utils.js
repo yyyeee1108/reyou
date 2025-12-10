9408fe8474d2c743e47e3af322a135db567706c6
Yelim Shin <yyyeee1108@gmail.com>
2025-12-10 16:45:09 +0900

    feat: 복습 주기 D-Day 계산 로직 구현
    - 다음 복습 날짜를 입력으로 받아 현재 날짜와 비교하여 D-Day를 반환하는
      함수 구현
    - 추후 UI에 'D-3', 'D-Day'('오늘 복습')과 같이 활용하여 표시

--- utils.js
+++ utils.js
@@ -1,7 +1,12 @@
 console.log('utils.js 실행');
 
-export const REVIEW_INTERVALS = [1, 4, 7, 14, 30];
+export const REVIEW_INTERVALS = [0, 1, 4, 7, 14, 30];
 
+/**
+ * 복습 단계를 입력으로 받아 다음 복습 날짜를 반환하는 함수
+ * @param {number} stage
+ * @returns {string} nextDate
+ */
 export function calculateNextDate(stage) {
   const interval = REVIEW_INTERVALS[stage];
 
@@ -11,4 +16,24 @@ export function calculateNextDate(stage) {
   nextDate.setDate(nextDate.getDate() + interval);
 
   return nextDate.toISOString();
+}
+
+/**
+ * 다음 복습 날짜를 입력으로 받아 D-Day를 반환하는 함수
+ * @param {string} targetDateStr
+ * @returns {number} D-Day
+ */
+export function calculateDday(targetDateStr) {
+  const targetDate = new Date(targetDateStr); // 다음 복습 날짜
+  const currentDate = new Date(); // 현재 날짜
+
+  // 날짜만 비교하도록 hours, min, sec, ms는 0으로 초기화
+  currentDate.setHours(0, 0, 0, 0);
+  targetDate.setHours(0, 0, 0, 0);
+
+  const difference = targetDate - currentDate;
+  const dDay = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+  console.log(`D-Day: ${dDay}`);
+  return dDay;
 }
